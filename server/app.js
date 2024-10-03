d4d39b8b08c66d9e4849e15d7bd1cbc310a26232
Chamara-Wijepala <mr.worf777@gmail.com>
2024-10-03 18:11:05 +0530

    Validate and provide feedback to user on form submit

--- server/app.js
+++ server/app.js
@@ -1,16 +1,36 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const { body, validationResult, matchedData } = require('express-validator');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/', (req, res) => {
-	const { name, email, message } = req.body;
+app.post(
+	'/',
+	[
+		body('name').trim().notEmpty().withMessage('Name is empty').escape(),
+		body('email')
+			.trim()
+			.notEmpty()
+			.withMessage('Email is empty')
+			.escape()
+			.isEmail()
+			.withMessage('Not a valid email'),
+		body('message').trim().notEmpty().withMessage('Message is empty').escape(),
+	],
+	(req, res) => {
+		const result = validationResult(req);
 
-	console.log({ name, email, message });
+		if (result.isEmpty()) {
+			const data = matchedData(req);
+			res.status(200).send(`Hello, ${data.name}.`);
+		}
 
-	res.send('Success!');
-});
+		res.status(400).send({ errors: result.array() });
+	}
+);
 
 app.listen(PORT, () => console.log('Server running on port:', PORT));
